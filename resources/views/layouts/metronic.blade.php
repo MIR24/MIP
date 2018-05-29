2437104c621619573145f16e843eda1b40e7266d
Konshin Alexander <kon2work@gmail.com>
2018-05-29 18:43:14 +0300

    change fonts Poppins to Open Sans

--- resources/views/layouts/metronic.blade.php
+++ resources/views/layouts/metronic.blade.php
@@ -25,7 +25,7 @@ License: You must have a valid license purchased only from themeforest(the above
         <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js"></script>
         <script>
           WebFont.load({
-            google: {"families":["Poppins:300,400,500,600,700","Roboto:300,400,500,600,700"]},
+            google: {"families":["Open Sans:300,400,500,600,700","Roboto:300,400,500,600,700"]},
             active: function() {
                 sessionStorage.fonts = true;
             }
