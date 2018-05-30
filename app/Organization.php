ed45b7685e378e283070b95f35fd4832b9239112
Konshin Alexander <kon2work@gmail.com>
2018-05-30 18:37:10 +0300

    make topic creation modal form work

--- app/Organization.php
+++ app/Organization.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Organization extends Model
 {
+    use SoftDeletes;
+
     /**
      * Get the users records associated with the organization.
      */
--- app/Video.php
+++ app/Video.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Video extends Model
 {
+    use SoftDeletes;
+
     /**
      * Get the topic record associated with the video.
      */
