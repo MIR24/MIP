3a2f0b593de5400ac33b72f74af87192018874f8
vanzi <ivanl487@gmail.com>
2018-05-31 15:05:44 +0300

    pagination fix

--- resources/views/datatables/topic.blade.php
+++ resources/views/datatables/topic.blade.php
@@ -188,12 +188,6 @@ var datatableTopics = function() {
                 responsive: {hidden: 'xl'},
             }]
         });
-
-        $('.m_datatable').on('click', 'tbody > tr', function (event) {
-            $("#m_modal_show_topic_cdn_video").append(makeVideoTag($(this).find("[data-field='video.cdn_cdn_url']").text(), $(this).find("[data-field='video.cdn_content_type']").text()));
-            $("#m_modal_show_topic_description_short").text($(this).find("[data-field='description_short']").text());
-            $("#m_modal_show_topic_btn").trigger("click");
-        })
     }
     // datatables
     datatableTopics();
