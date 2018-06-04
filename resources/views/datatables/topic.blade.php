9152dcc5406b938690a003412b4809754bc0ddd6
andrew <etovolchanka@gmail.com>
2018-06-04 17:38:33 +0300

    fix margin

--- resources/views/datatables/topic.blade.php
+++ resources/views/datatables/topic.blade.php
@@ -1,32 +1,28 @@
 @extends('layouts.metronic')
 
 @section('content')
-<div class="row">
-    <div class="col-xl-12">
-        <div class="m-portlet m-portlet--mobile ">
-            <div class="m-portlet__head">
-                <div class="m-portlet__head-caption">
-                    <div class="m-portlet__head-title">
-                        <h3 class="m-portlet__head-text">
-                            Сюжеты
-                        </h3>
-                    </div>
-                </div>
-            </div>
-            <div class="col-xl-6">
-                <label for="searchCreated_at">Искать по дате публикации:</label>
-                <input type="date" class="form-control m-input" id="searchCreated_at">
-            </div>
-            <div class="col-xl-6">
-                <label for="searchOrganization">Искать по организациям:</label>
-                <input type="text" class="form-control m-input" id="searchOrganization">
-            </div>
-            <div class="m-portlet__body">
-                <!--begin: Datatable -->
-                <div class="m_datatable" id="m_datatable_topics"></div>
-                <!--end: Datatable -->
+<div class="m-portlet m-portlet--mobile ">
+    <div class="m-portlet__head">
+        <div class="m-portlet__head-caption">
+            <div class="m-portlet__head-title">
+                <h3 class="m-portlet__head-text">
+                    Сюжеты
+                </h3>
             </div>
         </div>
+    </div>
+    <div class="col-xl-6">
+        <label for="searchCreated_at">Искать по дате публикации:</label>
+        <input type="date" class="form-control m-input" id="searchCreated_at">
+    </div>
+    <div class="col-xl-6">
+        <label for="searchOrganization">Искать по организациям:</label>
+        <input type="text" class="form-control m-input" id="searchOrganization">
+    </div>
+    <div class="m-portlet__body">
+        <!--begin: Datatable -->
+        <div class="m_datatable" id="m_datatable_topics"></div>
+        <!--end: Datatable -->
     </div>
 </div>
 @endsection
