0a6be00434dcc3268c3d04ad9f04488499bea6f1
Konshin Alexander <kon2work@gmail.com>
2018-06-06 18:37:14 +0300

    add range datepicker to backend

--- config/constants.php
+++ config/constants.php
@@ -3,5 +3,6 @@
 return [
 
     'topics_ttl' => env('CONSTANTS_TOPICS_TTL', 60), // in days
+    'datepicker_delimiter' => env('CONSTANTS_DATEPICKER_DELIMITER', ' - '),
 
 ];
--- resources/views/layouts/metronic.blade.php
+++ resources/views/layouts/metronic.blade.php
@@ -42,6 +42,7 @@ License: You must have a valid license purchased only from themeforest(the above
         <link href="{{ asset('assets/demo/demo2/base/style.bundle.css') }}" rel="stylesheet" type="text/css" />
         <!--end::Base Styles -->
         <link href="{{ asset('/css/core.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('/css/datepicker.min.css') }}" rel="stylesheet" type="text/css">
         <link rel="shortcut icon" href="{{ asset('assets/demo/demo2/media/img/logo/favicon.ico') }}" />
     </head>
     <!-- end::Head -->
@@ -177,6 +178,7 @@ License: You must have a valid license purchased only from themeforest(the above
     <!--begin::Page Snippets -->
     <script src="{{ asset('assets/app/js/dashboard.js') }}" type="text/javascript"></script>
     <script src="{{ asset('/js/core.js') }}"></script>
+    <script src="{{ asset('/js/datepicker.min.js') }}"></script>
     <!--end::Page Snippets -->
     @stack('scripts')
 </body>
