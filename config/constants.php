e4c3696a1031f38d87786d06de0dfce08303ee92
Konshin Alexander <kon2work@gmail.com>
2018-06-07 18:28:19 +0300

    change topic delete ttl to minutes

--- config/constants.php
+++ config/constants.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'topics_ttl' => env('CONSTANTS_TOPICS_TTL', 60), // in days
+    'topics_ttl' => env('CONSTANTS_TOPICS_TTL', 60 * 24 * 60), // in minutes
     'datepicker_delimiter' => env('CONSTANTS_DATEPICKER_DELIMITER', ' - '),
 
 ];
