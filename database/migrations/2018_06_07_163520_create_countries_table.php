8809cd701b05d3d3798e21801365b94ef53a5b9d
Konshin Alexander <kon2work@gmail.com>
2018-06-07 16:49:56 +0300

    add Counry model and migration

---
+++ database/migrations/2018_06_07_163520_create_countries_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->string('name');
+            $table->string('image_url', 2083)->nullable();
+        });
+
+        Schema::table('organizations', function (Blueprint $table) {
+            $table->bigInteger('country_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+
+        Schema::table('organizations', function (Blueprint $table) {
+            $table->dropColumn('country_id');
+        });
+    }
+}
