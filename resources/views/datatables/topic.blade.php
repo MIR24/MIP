5c826895e4b8c75a7a6aec4b8a6eaa1468eb833a
vanzi <ivanl487@gmail.com>
2018-06-09 14:18:27 +0300

    https://mir24tv.atlassian.net/browse/MIP-90 - inline search

--- resources/views/datatables/topic.blade.php
+++ resources/views/datatables/topic.blade.php
@@ -4,15 +4,19 @@
 <div class="row">
     <div class="col-xl-12">
         <div class="m-portlet m-portlet--mobile ">
-            <div class="col-xl-6">
-                <label for="searchCreated_atAir">Фильтр по диапозону дат:</label>
-                <input type="text" class="form-control m-input" id="searchCreated_atAir"/>
-                <i id="searchCreated_atAirClear" class="flaticon-cancel in-input-clear"></i>
-            </div>
-            <div class="col-xl-6">
-                <label for="searchOrganization">Искать по организациям:</label>
-                <input type="text" class="form-control m-input" id="searchOrganization">
-                <i id="searchOrganizationClear" class="flaticon-cancel in-input-clear"></i>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-xl-6">
+                        <label for="searchCreated_atAir">Фильтр по диапозону дат:</label>
+                        <input type="text" class="form-control m-input" id="searchCreated_atAir"/>
+                        <i id="searchCreated_atAirClear" class="flaticon-cancel in-input-clear"></i>
+                    </div>
+                    <div class="col-xl-6">
+                        <label for="searchOrganization">Искать по организациям:</label>
+                        <input type="text" class="form-control m-input" id="searchOrganization">
+                        <i id="searchOrganizationClear" class="flaticon-cancel in-input-clear"></i>
+                    </div>
+                </div>
             </div>
             <div class="m-portlet__body">
                 <!--begin: Nav tabs -->
