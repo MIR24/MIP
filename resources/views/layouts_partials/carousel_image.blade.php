b6b11966477b388032614d9edbd1d98d6e6e3e8e
andrew <etovolchanka@gmail.com>
2018-06-13 14:15:48 +0300

    remove poster titles

--- resources/views/layouts_partials/carousel_image.blade.php
+++ resources/views/layouts_partials/carousel_image.blade.php
@@ -1,13 +1,11 @@
 <a class="poster" href="#" style="background-image: url(/images/back.png)">
-    <div class="poster-content">
-        <div class="poster-titles">
+    {{--<div class="poster-content">--}}
+        {{--<div class="poster-titles">--}}
             {{--<div>{{$topic->description_short}}</div>--}}
-{{--            <div>{{$topic->description_long}}</div>--}}
-            <div>Таким образом, дальнейшее развитие приводит к пониманию числа {{$num}}</div>
-            <div>{{$num}} меньше, чем {{$num+1}}</div>
-        </div>
-        <div class="poster-arrow">➔</div>
-    </div>
+            {{--<div>{{$topic->description_long}}</div>--}}
+        {{--</div>--}}
+        {{--<div class="poster-arrow">➔</div>--}}
+    {{--</div>--}}
     <img class="mip-logo" src="/images/mip-logo.png" alt="First slide">
     <img class="d-block w-100 blue-cover" src="/images/mip.png" alt="First slide">
 </a>
