9968bc06f5973ea3e73124a5e28ef5cfcc39a5ab
Konshin Alexander <kon2work@gmail.com>
2018-06-13 13:02:54 +0300

    add custom 503 error page

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.front')
+
+@section('content')
+<div class="jumbotron" style="text-align: center;
+            height: 84vh;
+            align-items: center;
+            display: flex;
+            justify-content: center;
+            position: relative;">
+    <div class="display-4">
+        Cкоро!
+    </div>
+    <div class="lead">
+        Мы проводим последние настройки сервера, спасибо за Ваше терпение.
+    </div>
+</div>
+@endsection
