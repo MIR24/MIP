b62ee04de3bd5b5fba28ec838447a2a393f854a2
Konshin Alexander <kon2work@gmail.com>
2018-07-20 17:59:14 +0300

    remove invite link from organizations

--- resources/views/columns/organizations.blade.php
+++ resources/views/columns/organizations.blade.php
@@ -7,16 +7,5 @@
         @endif
         @include('columns_partials.organization', ['org' => $orgs[$i]])
     @endfor
-    <a href="#" class="ch ch-mip col-md-4">
-        <div class="cellwrap">
-            <div class="channel-logo-big">
-                <div class="country"></div>
-            </div>
-            <div class="advert">
-                <div>Правила присоединения к информационному пулу</div>
-                <div class="ch-arrow"></div>
-            </div>
-        </div>
-    </a>
     </div>
 </div>
